b63019deabcc3054a456ef2b2156e527b82f8278
kwcheung <keinwaicheung@gmail.com>
2026-08-02 23:12:14 +0200

    fix: bloquear zoom y rebote en iOS

--- client/capacitor.config.ts
+++ client/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.keinwaicheung.tttournamentapp',
-  appName: 'tttournamentapp',
+  appName: 'TT Tournament App',
   webDir: 'dist',
   server: {
     cleartext: true,
